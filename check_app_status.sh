bedd9311fc80f14545ddf82efdfc4bb50bb715c3
anajuliacbf <anajuliacbfernandes@gmail.com>
2024-11-13 09:30:48 -0300

    Adicionar o script de verificação de status da aplicação

---
+++ check_app_status.sh
@@ -0,0 +1,10 @@
+#!/bin/bash  declara que é um script bash
+# Usa o curl para verificar o status da aplicação
+STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8080)
+if [ $STATUS -eq 200 ]; then
+    echo "Application is up and running."
+else
+    echo "Application is down. Status code: $STATUS"
+    exit 1
+fi
+
